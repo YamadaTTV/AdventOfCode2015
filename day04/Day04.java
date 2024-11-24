d13a7effba471bfcf466959484f8dfd78ea62454
YamadaTTV <92035299+YamadaTTV@users.noreply.github.com>
2024-11-24 15:06:05 +0100

    Day 4 part 2 solution

--- day04/Day04.java
+++ day04/Day04.java
@@ -29,7 +29,31 @@ public class Day04 {
         }
     }
 
+    public static void part2() throws NoSuchAlgorithmException {
+        String secretKey = "bgvyzdsv";
+        int number = 1;
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        while(true){
+            String input = secretKey + number;
+            byte[] hash = md.digest(input.getBytes());
+            StringBuilder hexadecimalString = new StringBuilder();
+            for(byte b: hash){
+                String hexadecimal = Integer.toHexString(0xFF & b);
+                if(hexadecimal.length() == 1){
+                    hexadecimalString.append("0");
+                }
+                hexadecimalString.append(hexadecimal);
+            }
+            if(hexadecimalString.toString().startsWith("000000")){
+                System.out.println(number);
+                break;
+            }
+            number++;
+        }
+    }
+
     public static void main(String[] args) throws NoSuchAlgorithmException {
         part1();
+        part2();
     }
 }
