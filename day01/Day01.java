b326624eed28378c3f006dbfc969c534db5c4f54
YamadaTTV <92035299+YamadaTTV@users.noreply.github.com>
2024-11-24 12:04:27 +0100

    Day 1 part 2 solution

--- day01/Day01.java
+++ day01/Day01.java
@@ -27,7 +27,27 @@ public class Day01 {
         System.out.println(floor);
     }
 
+    public static void part2() throws IOException {
+        int floor = 0;
+        int charCount = 0;
+        String input = readInput("input/day01.txt");
+        char[] characters = input.toCharArray();
+        for(char c: characters){
+            if(c == '('){
+                floor++;
+            } else if(c == ')'){
+                floor--;
+            }
+            charCount++;
+            if(floor == -1){
+                System.out.println(charCount);
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         part1();
+        part2();
     }
 }
