6b2e783b9ba5100120a611875943b99b8f6ec5a1
YamadaTTV <92035299+YamadaTTV@users.noreply.github.com>
2024-11-24 12:48:57 +0100

    Day 2 part 1 solution

---
+++ day02/Day02.java
@@ -0,0 +1,55 @@
+package day02;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day02 {
+
+    public static List<String> readInput(String filePath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        List<String> res = new ArrayList<>();
+        String line = reader.readLine();
+        while(line != null){
+            res.add(line);
+            line = reader.readLine();
+        }
+        return res;
+    }
+
+    public static void part1() throws IOException {
+        List<String> input = readInput("input/day02.txt");
+        int length;
+        int width;
+        int height;
+        int surfaceArea;
+        int smallestSide = 0;
+        int totalPaper = 0;
+        for(int i = 0; i < input.size(); i++){
+            String[] split = input.get(i).split("x");
+            length = Integer.parseInt(split[0]);
+            width = Integer.parseInt(split[1]);
+            height = Integer.parseInt(split[2]);
+            surfaceArea = 2 * length * width + 2 * width * height + 2 * height * length;
+            int lw = length * width;
+            int wh = width * height;
+            int hl = height * length;
+            if(lw <= wh && lw <= hl){
+                smallestSide = lw;
+            } else if(wh <= lw && wh <= hl){
+                smallestSide = wh;
+            } else {
+                smallestSide = hl;
+            }
+            totalPaper += surfaceArea + smallestSide;
+        }
+        System.out.println(totalPaper);
+    }
+
+    public static void main(String[] args) throws IOException {
+        part1();
+    }
+}
